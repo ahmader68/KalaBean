472649ccdf0c4444e78b615b46958cbddaa56f1f
Hooman Hooshyar <hooman.hooshyari@gmail.com>
2019-08-24 17:25:00 +0430

    Change API service again

--- app/src/main/java/com/intek/kalabean/Data/ApiService.java
+++ app/src/main/java/com/intek/kalabean/Data/ApiService.java
@@ -1,10 +1,15 @@
 package com.intek.kalabean.Data;
 
+import java.util.List;
+
 import io.reactivex.Single;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface ApiService {
     @GET("index.aspx")
     Single<String> str();
+    @POST("login.aspx")
+    Single<List<String>> users();
 
 }
