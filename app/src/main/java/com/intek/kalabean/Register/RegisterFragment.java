aa9c5552fec32d9a59fa6541b8582c48b513726a
James <ahmader1368@gmail.com>
2019-08-25 13:46:28 +0430

    Back normal

---
+++ app/src/main/java/com/intek/kalabean/Register/RegisterFragment.java
@@ -0,0 +1,22 @@
+package com.intek.kalabean.Register;
+
+import android.content.Context;
+
+import com.intek.kalabean.Base.BaseFragment;
+
+public class RegisterFragment extends BaseFragment implements RegisterContract.View {
+    @Override
+    public int getLayout() {
+        return 0;
+    }
+
+    @Override
+    public void setupViews() {
+
+    }
+
+    @Override
+    public Context getViewContext() {
+        return null;
+    }
+}
