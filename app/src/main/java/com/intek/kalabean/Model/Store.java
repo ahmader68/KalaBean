f49752f7ee775526f7aefc0851917057c50395ef
Hooman Hooshyar <hooman.hooshyari@gmail.com>
2019-08-26 18:52:23 +0430

    Recycler views UI and models finished

---
+++ app/src/main/java/com/intek/kalabean/Model/Store.java
@@ -0,0 +1,130 @@
+package com.intek.kalabean.Model;
+
+public class Store {
+    private String img;
+    private String storeName;
+    private int storeCount;
+    private int floorCount;
+    private String address;
+    private boolean stair;
+    private boolean cafe;
+    private boolean wc;
+    private boolean gift;
+    private boolean parking;
+    private boolean play;
+    private boolean shop;
+    private boolean wifi;
+    private boolean elevator;
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public int getStoreCount() {
+        return storeCount;
+    }
+
+    public void setStoreCount(int storeCount) {
+        this.storeCount = storeCount;
+    }
+
+    public int getFloorCount() {
+        return floorCount;
+    }
+
+    public void setFloorCount(int floorCount) {
+        this.floorCount = floorCount;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean isStair() {
+        return stair;
+    }
+
+    public void setStair(boolean stair) {
+        this.stair = stair;
+    }
+
+    public boolean isCafe() {
+        return cafe;
+    }
+
+    public void setCafe(boolean cafe) {
+        this.cafe = cafe;
+    }
+
+    public boolean isWc() {
+        return wc;
+    }
+
+    public void setWc(boolean wc) {
+        this.wc = wc;
+    }
+
+    public boolean isGift() {
+        return gift;
+    }
+
+    public void setGift(boolean gift) {
+        this.gift = gift;
+    }
+
+    public boolean isParking() {
+        return parking;
+    }
+
+    public void setParking(boolean parking) {
+        this.parking = parking;
+    }
+
+    public boolean isPlay() {
+        return play;
+    }
+
+    public void setPlay(boolean play) {
+        this.play = play;
+    }
+
+    public boolean isShop() {
+        return shop;
+    }
+
+    public void setShop(boolean shop) {
+        this.shop = shop;
+    }
+
+    public boolean isWifi() {
+        return wifi;
+    }
+
+    public void setWifi(boolean wifi) {
+        this.wifi = wifi;
+    }
+
+    public boolean isElevator() {
+        return elevator;
+    }
+
+    public void setElevator(boolean elevator) {
+        this.elevator = elevator;
+    }
+}
