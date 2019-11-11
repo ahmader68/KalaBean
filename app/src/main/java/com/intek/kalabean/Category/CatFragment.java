def2823619fb2458246a23b40c3059d8fa3ba71e
Hooman Hooshyar <hooman.hooshyari@gmail.com>
2019-11-11 14:50:54 +0330

    Initialize category fragment's views

--- app/src/main/java/com/intek/kalabean/Category/CatFragment.java
+++ app/src/main/java/com/intek/kalabean/Category/CatFragment.java
@@ -7,6 +7,7 @@ import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.intek.kalabean.Base.BaseFragment;
 import com.intek.kalabean.Data.KalaBeanRepository;
@@ -49,6 +50,14 @@ public class CatFragment extends BaseFragment implements CatContract.View {
             view4,
             view5,
             view6;
+
+    private RecyclerView
+            rvClothFurniture,
+            rvJewelleryBeauty,
+            rvIndustryFood,
+            rvDigitalSport,
+            rvArtKid,
+            rvWedding;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -93,6 +102,13 @@ public class CatFragment extends BaseFragment implements CatContract.View {
         view4 = rootView.findViewById(R.id.view_fragmentCategory_line4);
         view5 = rootView.findViewById(R.id.view_fragmentCategory_line5);
         view6 = rootView.findViewById(R.id.view_fragmentCategory_line6);
+
+        rvClothFurniture = rootView.findViewById(R.id.rv_fragmentCategory_cloth_furniture);
+        rvArtKid = rootView.findViewById(R.id.rv_fragmentCategory_kid_art);
+        rvDigitalSport = rootView.findViewById(R.id.rv_fragmentCategory_sport_digital);
+        rvIndustryFood = rootView.findViewById(R.id.rv_fragmentCategory_food_industry);
+        rvJewelleryBeauty = rootView.findViewById(R.id.rv_fragmentCategory_beauty_jewellery);
+        rvWedding = rootView.findViewById(R.id.rv_fragmentCategory_wedding);
 
     }
 
